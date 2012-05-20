49aad0e9265c4098b3c0cca82e31ec6b0cc0da4d
Kevin <keverw92@gmail.com>
2012-05-19 23:28:46 -0400

    made test dir a variable.

--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,19 @@
 var basictemplate = require('../basictemplate.js');
 
-/*basictemplate.render('./templates/basic/layout.bt', {hello: '<i>W</i>orld'}, function(err, html)
+var dir = 'basic';
+
+/*basictemplate.render('./templates/' + dir + '/layout.bt', {hello: '<i>W</i>orld'}, function(err, html)
 {
     console.log(err);
     console.log(html);
 });
 */
 
-basictemplate.render_sub('./templates/basic/layout.bt', './templates/basic/subview.bt', {
+basictemplate.render_sub('./templates/' + dir + '/layout.bt', './templates/' + dir + '/subview.bt', {
     hello: 'World',
     what: 'subview'
 }, function (err, html)
 {
-    // console.log(err);
+    //console.log(err);
     console.log(html);
 });
