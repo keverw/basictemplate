42acdc6611f0eef171b92a5409c4668e2664d299
Kevin <keverw92@gmail.com>
2012-05-19 23:41:53 -0400

    Fixed path issues with tests.

--- tests/render_test.js
+++ tests/render_test.js
@@ -1,18 +1,18 @@
 var basictemplate = require('../basictemplate.js');
 var dir = 'basic';
-/*basictemplate.render('./templates/' + dir + '/layout.bt', {hello: '<i>W</i>orld'}, function(err, html)
+/*basictemplate.render('../templates/' + dir + '/layout.bt', {hello: '<i>W</i>orld'}, function(err, html)
 {
     console.log(err);
     console.log(html);
 });
 */
 
-basictemplate.render_sub('./templates/' + dir + '/layout.bt', './templates/' + dir + '/subview.bt', {
+basictemplate.render_sub('../templates/' + dir + '/layout.bt', '../templates/' + dir + '/subview.bt', {
     hello: 'World',
     what: 'subview'
 }, function (err, html)
 {
-    // console.log(err);
+    console.log(err);
     // console.log(html);
     save2file(html);
 });
@@ -20,7 +20,7 @@ basictemplate.render_sub('./templates/' + dir + '/layout.bt', './templates/' + d
 function save2file(html)
 {
     var fs = require('fs');
-    fs.writeFile('./demo.html', html, 'utf8', function (err)
+    fs.writeFile('../demo.html', html, 'utf8', function (err)
     {
         if (err)
         {
--- tests/test.js
+++ tests/test.js
@@ -2,14 +2,14 @@ var basictemplate = require('../basictemplate.js');
 
 var dir = 'basic';
 
-/*basictemplate.render('./templates/' + dir + '/layout.bt', {hello: '<i>W</i>orld'}, function(err, html)
+/*basictemplate.render('../templates/' + dir + '/layout.bt', {hello: '<i>W</i>orld'}, function(err, html)
 {
     console.log(err);
     console.log(html);
 });
 */
 
-basictemplate.render_sub('./templates/' + dir + '/layout.bt', './templates/' + dir + '/subview.bt', {
+basictemplate.render_sub('../templates/' + dir + '/layout.bt', '../templates/' + dir + '/subview.bt', {
     hello: 'World',
     what: 'subview'
 }, function (err, html)
