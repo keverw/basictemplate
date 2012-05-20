7ef0e84af83a6e1c331f9cd8068342b0de1631cb
Kevin <keverw92@gmail.com>
2012-05-19 23:25:35 -0400

    Renamed view.bt to layout.bt

--- tests/render_test.js
+++ tests/render_test.js
@@ -1,13 +1,13 @@
 var basictemplate = require('../basictemplate.js');
 
-/*basictemplate.render('./templates/basic/view.bt', {hello: '<i>W</i>orld'}, function(err, html)
+/*basictemplate.render('./templates/basic/layout.bt', {hello: '<i>W</i>orld'}, function(err, html)
 {
     console.log(err);
     console.log(html);
 });
 */
 
-basictemplate.render_sub('./templates/basic/view.bt', './templates/basic/subview.bt', {
+basictemplate.render_sub('./templates/basic/layout.bt', './templates/basic/subview.bt', {
     hello: 'World',
     what: 'subview'
 }, function (err, html)
