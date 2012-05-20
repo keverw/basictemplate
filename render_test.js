0eca464f14f0a6a3704c84403de54cb6ac07d880
Kevin <keverw92@gmail.com>
2012-05-19 23:18:11 -0400

    Moved templates to it's own dir.

--- render_test.js
+++ render_test.js
@@ -1,27 +1,34 @@
 var basictemplate = require('./basictemplate.js');
 
-/*basictemplate.render('./view.bt', {hello: '<i>W</i>orld'}, function(err, html)
+/*basictemplate.render('./templates/basic/view.bt', {hello: '<i>W</i>orld'}, function(err, html)
 {
     console.log(err);
     console.log(html);
 });
 */
 
-basictemplate.render_sub('./view.bt', './subview.bt', {hello: 'World', what: 'subview'}, function(err, html)
+basictemplate.render_sub('./templates/basic/view.bt', './templates/basic/subview.bt', {
+    hello: 'World',
+    what: 'subview'
+}, function (err, html)
 {
-   // console.log(err);
+    // console.log(err);
     console.log(html);
     save2file(html);
 });
 
 function save2file(html)
 {
-	var fs = require('fs');
-	fs.writeFile('./demo.html', html, 'utf8', function(err) {
-    	if(err) {
-        	console.log(err);
-    	} else {
-        	console.log("The file was saved!");
-    	}
-	}); 
+    var fs = require('fs');
+    fs.writeFile('./demo.html', html, 'utf8', function (err)
+    {
+        if (err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("The file was saved!");
+        }
+    });
 }
--- test.js
+++ test.js
@@ -1,14 +1,17 @@
 var basictemplate = require('./basictemplate.js');
 
-/*basictemplate.render('./view.bt', {hello: '<i>W</i>orld'}, function(err, html)
+/*basictemplate.render('./templates/basic/view.bt', {hello: '<i>W</i>orld'}, function(err, html)
 {
     console.log(err);
     console.log(html);
 });
 */
 
-basictemplate.render_sub('./view.bt', './subview.bt', {hello: 'World', what: 'subview'}, function(err, html)
+basictemplate.render_sub('./templates/basic/view.bt', './templates/basic/subview.bt', {
+    hello: 'World',
+    what: 'subview'
+}, function (err, html)
 {
-   // console.log(err);
+    // console.log(err);
     console.log(html);
 });
